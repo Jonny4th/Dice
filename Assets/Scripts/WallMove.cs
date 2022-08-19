cb2064837306c7dba81dcb57cc7fe93c01d03317
Jonny4th <91357144+Jonny4th@users.noreply.github.com>
2022-08-19 11:18:53 +0700

    Refractor WallMove

--- Assets/Scripts/WallMove.cs
+++ Assets/Scripts/WallMove.cs
@@ -5,7 +5,7 @@ public class WallMove : MonoBehaviour
 {
     [SerializeField] private FloatVariable posRef;
     private float posCache;
-    [SerializeField] private float waitTime;
+    [SerializeField] private float t;
 
     void Start()
     {
@@ -15,22 +15,9 @@ public class WallMove : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (posCache != posRef.Value)
+        if (transform.position != -posRef.Value/2 * transform.forward)
         {
-            StartCoroutine(Move(transform.position, -posRef.Value/2 * transform.forward));
-        }
-    }
-
-    IEnumerator Move(Vector3 start, Vector3 destination)
-    {
-        posCache = posRef.Value;
-        var t = 0f;
-        while (t < waitTime)
-        {
-            transform.position = Vector3.Lerp(start, destination, t/waitTime);
-            t += Time.deltaTime;
-            if (t > waitTime) t = waitTime;
-            yield return null;
+            transform.position = Vector3.Lerp(transform.position, -posRef.Value/2 * transform.forward, t);
         }
     }
 }
